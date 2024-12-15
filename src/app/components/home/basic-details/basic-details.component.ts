e5a9a868980fd805bddecdf1b453f2c6d6bd1330
Satrughna <thisissatrughna@gmail.com>
2024-12-15 17:30:47 +0530

    refactor: unnecessary icons import removed

--- src/app/components/home/basic-details/basic-details.component.ts
+++ src/app/components/home/basic-details/basic-details.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { IconEmailComponent } from '../../icons/icon-email/icon-email.component';
-import { IconMobileComponent } from '../../icons/icon-mobile/icon-mobile.component';
-import { IconAddressComponent } from '../../icons/icon-address/icon-address.component';
 
 @Component({
   selector: 'basic-details',
-  imports: [IconEmailComponent, IconMobileComponent, IconAddressComponent],
+  imports: [],
   templateUrl: './basic-details.component.html',
   styleUrl: './basic-details.component.css',
 })
