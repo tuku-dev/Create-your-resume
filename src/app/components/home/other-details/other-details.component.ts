044913aacd34be70cb1d1bd3ef57b58f931c8bb8
Satrughna <thisissatrughna@gmail.com>
2024-12-15 18:07:42 +0530

    feat: other details added, editor added

--- src/app/components/home/other-details/other-details.component.ts
+++ src/app/components/home/other-details/other-details.component.ts
@@ -1,11 +1,52 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {
+  AngularEditorConfig,
+  AngularEditorModule,
+} from '@kolkov/angular-editor';
 
 @Component({
-  selector: 'app-other-details',
-  imports: [],
+  selector: 'other-details',
+  imports: [HttpClientModule, AngularEditorModule],
   templateUrl: './other-details.component.html',
-  styleUrl: './other-details.component.css'
+  styleUrl: './other-details.component.css',
 })
 export class OtherDetailsComponent {
-
+  editorConfig: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '300px',
+    minHeight: '0',
+    maxHeight: 'auto',
+    width: 'auto',
+    minWidth: '0',
+    translate: 'yes',
+    enableToolbar: true,
+    showToolbar: true,
+    placeholder: 'Enter text here...',
+    defaultParagraphSeparator: '',
+    defaultFontName: '',
+    defaultFontSize: '',
+    fonts: [
+      // { class: 'arial', name: 'Arial' },
+      // { class: 'times-new-roman', name: 'Times New Roman' },
+      // { class: 'calibri', name: 'Calibri' },
+      // { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+    ],
+    customClasses: [
+      // {
+      //   name: 'quote',
+      //   class: 'quote',
+      // },
+      // {
+      //   name: 'redText',
+      //   class: 'redText',
+      // },
+      // {
+      //   name: 'titleText',
+      //   class: 'titleText',
+      //   tag: 'h1',
+      // },
+    ],
+  };
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BasicDetailsComponent } from '../../components/home/basic-details/basic-details.component';
+import { OtherDetailsComponent } from "../../components/home/other-details/other-details.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BasicDetailsComponent],
+  imports: [BasicDetailsComponent, OtherDetailsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
